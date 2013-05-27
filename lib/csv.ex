ebef890574af1a98016dbf6b3b938759f0958320
Geoffrey Clements <baldmountain@gmail.com>
2013-05-27 18:26:18 -0400

    Add parse_file

--- lib/csv.ex
+++ lib/csv.ex
@@ -7,27 +7,40 @@ defmodule CSV do
 	@quoted_state 1
 
 	@doc "Parse a bunch of CSV and return a list of lines parsed as csv"
-	def parse(s, separator // ",", quote_char // "\"") do
-		{ word, line, list, state } = String.codepoints(s) |> Enum.reduce({ "", [], [], @normal_state }, fn c, { word, line, list, state } ->
+	def parse(s, separator // ",", quote_char // "\"", skip_lines // 0) do
+		{ word, line, list, state, skip_lines } = String.codepoints(s) |> Enum.reduce({ "", [], [], @normal_state, skip_lines }, fn c, { word, line, list, state, skip_lines } ->
 			case c do
-				"\n" ->
+				"\n" when skip_lines == 0 ->
 					list = list ++ [List.flatten([line, word])]
 					word = ""
 					line = []
-				^separator when state == @normal_state ->
+				"\n" when skip_lines > 0 ->
+					skip_lines = skip_lines - 1
+				"\r" ->
+					nil
+				^separator when state == @normal_state and skip_lines == 0 ->
 					line = [line, word]
 					word = ""
-				^quote_char when state == @quoted_state ->
+				^quote_char when state == @quoted_state and skip_lines == 0 ->
 					state = @normal_state
-				^quote_char when state != @quoted_state ->
+				^quote_char when state != @quoted_state and skip_lines == 0 ->
 					state = @quoted_state
-				_ -> word = word <> c
+				_  ->
+					if skip_lines == 0, do: word = word <> c
 			end
-			{ word, line, list, state }
+			{ word, line, list, state, skip_lines }
 		end)
 		line = List.flatten([line, word])
 		list = list ++ [line]
 		list
 	end
-	
+
+	@doc "Parse a file and return a list of lines parsed as CVS"
+	def parse_file(filename, separator // ",", quote_char // "\"", skip_lines // 0) do
+		{ status, body } = File.read(filename)
+		case status do
+			:ok -> parse(body, separator, quote_char, skip_lines)
+			_ -> []
+		end
+	end
 end
--- test/csv_test.exs
+++ test/csv_test.exs
@@ -8,5 +8,10 @@ defmodule CsvTest do
   		assert(CSV.parse("\"FB\",\"Facebook, Inc.\"\n\"FB\",\"Facebook, Inc.\"") == [["FB", "Facebook, Inc."],["FB", "Facebook, Inc."]])
   		long_csv = "one,two,,three,,,four\n,five,six,,,seven,eight\nnine,ten,eleven,twelve,thirteen,fourteen,fifteen\n,,,,,,"
   		assert(inspect CSV.parse(long_csv) == [["one","two","","three","","","four"],["","five","six","","","seven","eight"],["nine","ten","eleven","twelve","thirteen","fourteen","fifteen"],["","","","","","",""]])
+
+  		assert CSV.parse_file("foo.csv") == []
+
+  		assert Enum.at(CSV.parse_file("LicenseImportSample.csv"), 0) == ["SRM_SaaS_ES","TLSMLICES","AddChange","EN"]
+  		assert Enum.at(CSV.parse_file("LicenseImportSample.csv"), 2) == ["3.0","2.0","5","INSTINST","","DHL","DAILY","","","2010-07-20T00:00:00-04:00","Sample License","6200-323-000","0","TLSM License 1 Sample","TLSMLICENSE1","ENTERPRISE","EXECUTED","GENERIC","","EAGLENA","DISTRIBUTED","","","","","","2010-07-20T00:00:00-04:00","DRAFT","","","Microsoft Corporation","3D Pinball 5","","5.1.2600.0","2","","","","","","","","","","","","","",""]
   	end
 end
