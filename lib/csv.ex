424baac340bb2c465007a344331ae50ad42efca9
Geoffrey Clements <gclements@ruelala.com>
2013-06-13 07:20:02 -0400

    try using parameterized functions instead of a cond

--- lib/csv.ex
+++ lib/csv.ex
@@ -6,45 +6,49 @@ defmodule CSV do
   '\r' newlines.
   """
 
-  @normal_state 0
-  @quoted_state 1
+  @normal 0
+  @quoted 1
 
   defrecord State,
     word: "",
     line: [],
     list: [],
-    state: @normal_state,
+    quoted: @normal_state,
     skip_lines: 0
+
+  # def handle_char(c, state, skip_lines, quoted), do:
+  def handle_char(c, state, 0, _), do: state.word(state.word <> c)
+  def handle_char(^quote_char, state, 0, @normal_state), do: state.quoted(@quoted_state)
+  def handle_char(^quote_char, state, 0, @quoted_state), do: state.quoted(@normal_state)
 
   @doc "Parse a bunch of CSV and return a list of lines parsed as csv"
   def parse(s, separator // ",", quote_char // "\"", skip_lines // 0) do
-    # list and line are built reversed since it is mre effieince to append to
+    # list and line are built reversed since it is more efficient to append to
     # head. They are revered at the end
     state = String.codepoints(s)
     |> Enum.reduce(State[skip_lines: skip_lines], fn c, state ->
       # define thes since we need then in guards
       skip_lines = state.skip_lines
-      current_state = state.state
+      current_state = state.quoted
       case c do
         "\n" when skip_lines == 0 ->
           state = state.list([Enum.reverse([state.word | state.line]) | state.list])
           state = state.word("")
-          state = state.line([])
+          state.line([])
         "\n" when skip_lines > 0 ->
-          state = state.skip_lines(state.skip_lines - 1)
+          state.skip_lines(state.skip_lines - 1)
         "\r" ->
           nil
         ^separator when current_state == @normal_state and skip_lines == 0 ->
           state = state.line([state.word | state.line])
-          state = state.word("")
+          state.word("")
         ^quote_char when current_state == @quoted_state and skip_lines == 0 ->
-          state = state.state(@normal_state)
+          handle_char(c, state, state.skip_lines, state.quoted)
         ^quote_char when current_state != @quoted_state and skip_lines == 0 ->
-          state = state.state(@quoted_state)
+          handle_char(c, state, state.skip_lines, state.quoted)
         _  ->
-          if state.skip_lines == 0, do: state = state.word(state.word <> c)
+          handle_char(c, state, state.skip_lines, state.quoted)
       end
-      state
     end)
     line = Enum.reverse([state.word | state.line])
     list = case line do
