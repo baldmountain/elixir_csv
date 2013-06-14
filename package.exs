870587e0403df93b63bfb22aeecf42887ad7688a
Geoffrey Clements <baldmountain@gmail.com>
2013-06-13 22:18:19 -0400

    Add an expm.co package file

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "elixir_csv", description: "A simple CSV parser.",
+                 version: "0.1.0", keywords: ["CSV"], 
+                 maintainers: [[name: "Geoffrey Clements", 
+                                email: "baldmountain@gmail.com"]],
+                 repositories: [[github: "baldmountain/elixir_csv"]])
+    
